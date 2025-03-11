ba47e9c9eb0e263f720caca6391d57fcf6def84c
moreen karimi <moreenk541@gmail.com>
2025-03-11 09:33:55 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution in this file!
+
+function distanceFromHqInBlocks(distance){
+   let hq = 42
+return Math.abs(distance-hq)
+}
+console.log(distanceFromHqInBlocks(43))
+
+function distanceFromHqInFeet(distance){
+let dist = 42
+  return Math.abs(distance-dist)*264
+}
+console.log(distanceFromHqInFeet(43))
+
+function distanceTravelledInFeet(start,destination){
+  return Math.abs(destination-start)*264
+}
+console.log(47,42)
+
+function calculatesFarePrice(start,destination){
+  const distance=Math.abs(destination-start)*264
+  if(distance<=400){
+    return 0
+  }
+  else if(distance>400&&distance<=2000){
+  return (distance-400)*0.02
+  }
+  else if(distance>2000&&distance<=2500){
+    return 25
+  }
+  else{
+    return `cannot travel that far`
+  }
+  }
